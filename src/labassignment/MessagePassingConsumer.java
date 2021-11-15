222b834b7fc6d2dda47cabe8d2413671d17b1a36
JoudMG <89661060+JoudMG@users.noreply.github.com>
2021-11-15 16:04:05 +0300

    Update MessagePassingConsumer.java

--- src/labassignment/MessagePassingConsumer.java
+++ src/labassignment/MessagePassingConsumer.java
@@ -1,13 +1,5 @@
 package labassignment;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * This is the consumer thread for the bounded buffer problem.
- */
 import java.util.*;
 
 class MessagePassingConsumer implements Runnable {
